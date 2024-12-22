2488491a7321af8d5d478a289563f5cdaa908e2c
Maks5962 <mdelvinquier@gmail.com>
2024-12-22 14:48:56 +0100

    modification email

--- api/sendEmail.js
+++ api/sendEmail.js
@@ -5,25 +5,26 @@ export default async function handler(req, res) {
     return res.status(405).send('Method Not Allowed');
   }
 
-  const { name, email, message } = req.body;
+  const { name, subject, message } = req.body;
 
   const transporter = nodemailer.createTransport({
-    service: 'gmail', // Utilisez votre fournisseur de messagerie
+    host: 'smtp-relay.brevo.com',
+    port: 587,
     auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
+      user: process.env.BREVO_EMAIL,
+      pass: process.env.BREVO_API_KEY,
     },
   });
 
   try {
     await transporter.sendMail({
-      from: process.env.EMAIL_USER,
-      to: 'maxence.delvinquier@climatep.fr', // Adresse email où les messages doivent être envoyés
+      from: 'mdelvinquier@gmail.com',
+      to: 'maxence.delvinquier@gmail.com', // L'adresse qui recevra les e-mails
       subject: `Message de ${name}`,
-      text: `De : ${email}\n\nMessage :\n${message}`,
+      text: `Objet du massage : ${subject} \n\nMessage :\n${message}`,
     });
 
-    res.status(200).json({ success: true, message: 'Email envoyé !' });
+    res.status(200).json({ success: true, message: 'Email envoyé avec succès !' });
   } catch (error) {
     console.error('Erreur lors de l\'envoi de l\'email :', error);
     res.status(500).json({ success: false, error: 'Erreur lors de l\'envoi de l\'email.' });
